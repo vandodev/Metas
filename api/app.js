153ef0423679e6d6763f9275462a3b19a2819398
Evandro oliveira <vandoaparecido@hotmail.com>
2021-01-04 18:31:18 -0300

    Trabalhando com CORS

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 require('./models/Metas');
 const Meta = mongoose.model('Meta');
@@ -7,6 +8,14 @@ const Meta = mongoose.model('Meta');
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) =>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER,Content-Type,Authorization");
+    app.use(cors());
+    next();
+});
 
 mongoose.connect('mongodb://localhost/metas', {
     useNewUrlParser: true,
