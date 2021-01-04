1098387379fe93d877b38217282498144273a667
Evandro oliveira <vandoaparecido@hotmail.com>
2021-01-04 12:07:04 -0300

    Instalando mongodb e mongoose

--- api/app.js
+++ api/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+app.get('/', async (req, res) => {
+   return res.json({
+       name:"Hello World"
+   })
 });
 
 app.listen(3000, () =>{
