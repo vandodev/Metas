0c7f35b82d77b117e6e2838a29e2e0f3f2aeac1d
Evandro oliveira <vandoaparecido@hotmail.com>
2021-01-05 09:15:10 -0300

    Exibindo dados da api

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,11 +1,26 @@
-function Home() {
-    return <div>Welcome to Next.js!</div>
+function Home({data}) {
+    return (
+      <>
+      
+        <h1>Minhas metas</h1> <hr />
+        <p>{console.log(data.metas)}</p>
+        {data.metas.map(meta => (
+            <div key={meta._id}>
+              <h2>{meta.name}</h2>
+              <p>{meta.description}</p>
+              <p>{meta.status}</p>
+              <hr />
+            </div>
+        ))}
+        
+      </>
+    );
 };
 
 export async function getServerSideProps() {
  const response = await fetch('http://localhost:8080/metas');
  const data = await response.json();
-  console.log(data);
+  //console.log(data);
   return {props: { data }};
 };
   
