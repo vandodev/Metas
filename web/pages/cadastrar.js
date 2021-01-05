1a9da3938dd197fb2d9a5f6cccc2b442cfe8c23d
Evandro oliveira <vandoaparecido@hotmail.com>
2021-01-05 09:36:45 -0300

    Criando formulário de cadastro

--- web/pages/cadastrar.js
+++ web/pages/cadastrar.js
@@ -3,6 +3,21 @@ function Cadastrar(){
         <>
             <h1>Cadastrar Meta</h1>
             <hr/>
+
+            <form>
+                <label>Nome: </label>
+                <input type="text" name="name" id="name" placeholder="Nome da meta" /> <br /><br />
+
+                <label>Descrição: </label>
+                <input type="text" name="description" id="description" placeholder="Descrição da meta" /> <br /><br />
+
+                <label>Status: </label>
+                <input type="text" name="status" id="status" placeholder="Status da meta" /> <br /><br />
+
+                <button type="submit">Cadastrar</button>
+
+            </form>
+
         </>
     ); 
 }
