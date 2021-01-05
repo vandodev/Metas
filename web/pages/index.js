228e0691341c18d42e79f912046549a5df20e365
Evandro oliveira <vandoaparecido@hotmail.com>
2021-01-05 14:41:46 -0300

    Estilizando listagem com Bootstrap

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,22 +1,50 @@
 import React from 'react';
 import Menu from '../components/Menu';
 
+import {Jumbotron, Container,ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
+
 function Home({data}) {
     return (
       <>
 
-        <Menu />
-      
-        <h1>Minhas metas</h1> <hr />
-        {data.metas.map(meta => (
-            <div key={meta._id}>
-              <h2>{meta.name}</h2>
-              <p>{meta.description}</p>
-              <p>{meta.status}</p>
-              <hr />
-            </div>
-        ))}
-        
+          <Menu />
+
+        <Jumbotron fluid className="list">
+          <Container>
+          
+          <style>
+            {`
+              .list{
+                background-color: #171941;
+                padding-top:30px;
+                padding-botton:150px;
+                margin-bottom: 0rem !important;
+              }
+              .title-top{
+                color: #bf38ac;
+              }
+              .list-meta{
+                background-color: #0d345d !important;
+                border-color: #4a0242 !important;
+                color: #fff;
+              }
+            `}           
+          </style>
+
+            <h1 className="display-4 text-center title-top">Minhas metas</h1> <hr />
+            <ListGroup>
+              {data.metas.map(meta => (
+                  <div key={meta._id}>
+                    <ListGroupItem className="list-meta">
+                      <ListGroupItemHeading>{meta.name}</ListGroupItemHeading>
+                      <ListGroupItemText>{meta.description}</ListGroupItemText>
+                      <ListGroupItemText>{meta.status}</ListGroupItemText>
+                    </ListGroupItem>
+                  </div>
+              ))}
+            </ListGroup>
+          </Container>
+        </Jumbotron>  
       </>
     );
 };
