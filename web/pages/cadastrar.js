d3e5cceebc1f95d86d782d1e5bca3414e2ee2c59
Evandro oliveira <vandoaparecido@hotmail.com>
2021-01-05 10:18:35 -0300

    Capturando dados do formulário

--- web/pages/cadastrar.js
+++ web/pages/cadastrar.js
@@ -1,18 +1,34 @@
+import React , {useState} from 'react';
+
 function Cadastrar(){
+
+    const [meta, setMeta] = useState({
+        name: '',
+        description: '',
+        status: ''
+    });
+
+    const onChangeInput = e => setMeta({...meta, [e.target.name]: e.target.value })
+
+    const sendMeta = async e =>{
+        e.preventDefault();
+        console.log(meta);
+    };
+
     return (
         <>
             <h1>Cadastrar Meta</h1>
             <hr/>
 
-            <form>
+            <form onSubmit={sendMeta}>
                 <label>Nome: </label>
-                <input type="text" name="name" id="name" placeholder="Nome da meta" /> <br /><br />
+                <input type="text" name="name" id="name" placeholder="Nome da meta" onChange={onChangeInput} /> <br /><br />
 
                 <label>Descrição: </label>
-                <input type="text" name="description" id="description" placeholder="Descrição da meta" /> <br /><br />
+                <input type="text" name="description" id="description" placeholder="Descrição da meta" onChange={onChangeInput} /> <br /><br />
 
                 <label>Status: </label>
-                <input type="text" name="status" id="status" placeholder="Status da meta" /> <br /><br />
+                <input type="text" name="status" id="status" placeholder="Status da meta" onChange={onChangeInput} /> <br /><br />
 
                 <button type="submit">Cadastrar</button>
 
